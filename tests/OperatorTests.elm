a97465bc2192f94f2cc7948499aaa06be6cf8664
Liam McCartney <liam.mccartney@gmail.com>
2020-06-24 13:55:16 -0400

    Create Operator Module

---
+++ tests/OperatorTests.elm
@@ -0,0 +1,44 @@
+module OperatorTests exposing (..)
+
+import Expect exposing (Expectation, FloatingPointTolerance(..))
+import Operator exposing (..)
+import Test exposing (..)
+
+
+suite : Test
+suite =
+    describe "The Operator Module"
+        [ describe "calculate"
+            [ test
+                "It performs addition"
+                (\_ ->
+                    calculate Add 4 5 |> Expect.equal 9
+                )
+            , test "It performs substraction"
+                (\_ ->
+                    calculate Subtract 4 5 |> Expect.equal -1
+                )
+            , test "It performs multiplication"
+                (\_ ->
+                    calculate Multiply 4 5 |> Expect.equal 20
+                )
+            , test "It performs integer division"
+                (\_ ->
+                    calculate Divide 4 5 |> Expect.equal 0
+                )
+            ]
+        , describe "findPrecedentOperators"
+            [ test "It returns all the operators from a stack that take precedence over a given operator"
+                (\_ ->
+                    findPrecedentOperators [ Multiply, Divide, Add, Subtract ] Divide
+                        |> Expect.equal [ Multiply, Divide ]
+                )
+            ]
+        , describe "findLesserOperators"
+            [ test "It returns all the operators from a stack that do not takes precedence over a given operator"
+                (\_ ->
+                    findLesserOperators [ Multiply, Divide, Subtract, Add ] Divide
+                        |> Expect.equal [ Subtract, Add ]
+                )
+            ]
+        ]
